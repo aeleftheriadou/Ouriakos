97e300acdd3a0c97ea6d4da341f6898a05e7d663
anasiagre@hotmail.com <A19961818a>
2022-05-19 14:41:20 +0100

    1st

---
+++ code.R
@@ -0,0 +1,15 @@
+devtools::install_github('benmarwick/rrtools', force=TRUE)
+library(rrtools)
+
+use_compendium()
+usethis::use_mit_license(copyright_holder = 'Anastasia Eleftheriadou')
+rrtools::use_readme_rmd()
+rrtools::use_analysis()
+
+
+
+
+
+
+
+
